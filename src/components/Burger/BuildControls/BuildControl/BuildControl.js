1bdf5a56892095feb619a84f50dda3d0288a849b
Kamran Ali <kamranalinitb@gmail.com>
2019-06-19 09:43:44 +0530

    Addding Build Control + Adding Ingredients

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './BuildControl.module.css';
+
+const BuildControl = (props) => {
+  const {label, added} = props;
+
+  return (
+    <div className={classes.BuildControl} >
+      <div className={classes.Label}>{label}</div>
+      <button className={classes.Less}>Less</button>
+      <button className={classes.More} onClick={added} >More</button>
+    </div>
+  );
+}
+
+export default BuildControl;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese:0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
 
@@ -10,7 +18,26 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
+  }
+
+  addIngredientHandler = (type) => {
+
+    const {ingredients, totalPrice} = this.state;
+    const oldCount = ingredients[type];
+    const updatedCount = oldCount + 1;
+
+    const updatedIngredient = {
+      ...ingredients
+    };
+
+    updatedIngredient[type] = updatedCount;
+
+    const priceAdditon = INGREDIENT_PRICES[type];
+    const newPrice = totalPrice + priceAdditon;
+
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredient});
   }
 
   render () {
@@ -18,7 +45,8 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={ingredients} />
-        <div>Build Controls</div>
+        <BuildControls 
+          ingredientAdded={this.addIngredientHandler}/>
       </Aux>
       
     );
