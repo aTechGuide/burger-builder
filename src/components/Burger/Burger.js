73ed2994d6953107d76338187cd9b9778f785806
Kamran Ali <kamranalinitb@gmail.com>
2019-06-19 07:06:16 +0530

    displaying message for 0 ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import classes from './Burger.module.css'
 
 const Burger = (props) => {
   const {ingredients} = props;
-  const transformedIngredients = Object.keys(ingredients)
+  let transformedIngredients = Object.keys(ingredients)
     .map(igKey => {
       return [...Array(ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       });
-    });
+    })
+    .reduce((arr, el) => {return arr.concat(el)}, []);
+
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please Start Adding Ingredients</p>
+    }
   
   return (
     <div className={classes.Burger}>
