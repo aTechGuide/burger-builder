d0dbbed398e56a32994b5f7099e76070af083764
Kamran Ali <kamranalinitb@gmail.com>
2019-06-20 09:11:01 +0530

    Adding a Backdrop to the modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,6 +87,10 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
   render () {
     const {ingredients, totalPrice, purchasable, purchasing} = this.state;
 
@@ -100,7 +104,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={purchasing}>
+        <Modal show={purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={ingredients}/>
         </Modal>
         <Burger ingredients={ingredients} />
