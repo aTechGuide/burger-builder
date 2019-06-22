35a787e8fc1eb150dfeb3865f8cca42985909f4d
Kamran Ali <kamranalinitb@gmail.com>
2019-06-22 18:36:36 +0530

    Adding Firebase to burger builder

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 ## Burger Builder
 
-React App to build a Burger dynamically. This App has following components
+Single page React App to build a Burger dynamically. This App has following components
 - Burger builder page taking inputs from user to build a burger
+- Fire base integration to persist the data
+
+## Tech Stack
+- React 
+- Firebase
+
+## Environment Variables
+Burger Builder uses environment variables to store sensitive information.  
+
+Please provide following environment variables to access full functionality
+- REACT_APP_DB_URL
 
 ## Resources
 - This app is build as a project of [React the complete guide incl Redux](https://www.udemy.com/react-the-complete-guide-incl-redux/) Udemy course
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -23,7 +25,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
-    purchasingFalse: false
+    purchasingFalse: false,
+    loading: false
   }
 
   updatePurchaseState = (ingredients) => {
@@ -92,28 +95,57 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You Continue!');
+    
+    this.setState({loading: true});
+
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Kamran Ali',
+        email: 'test@test.com',
+        address: {
+          street: 'Sector 43',
+          city: 'Gurugram',
+          zipCode: '122002'
+        },
+        deliveryMethod: 'fastest'
+      }
+    }
+    axios.post('/orders.json', order)
+    .then(response => {
+      this.setState({loading: false, purchasing: false});
+    })
+    .catch(error => {
+      this.setState({loading: false, purchasing: false});
+    });
   }
 
   render () {
-    const {ingredients, totalPrice, purchasable, purchasing} = this.state;
+    const {ingredients, totalPrice, purchasable, purchasing, loading} = this.state;
 
     const disabledInfo = {
       ...ingredients
     }
 
+    let orderSummary = <OrderSummary 
+      ingredients={ingredients} 
+      purchaseCancelled={this.purchaseCancelHandler}
+      purchaseContinued={this.purchaseContinueHandler} 
+      price={totalPrice}/>;
+
     for (const key in disabledInfo) {
       disabledInfo[key] = disabledInfo[key] <= 0
+    }
+
+    if (loading) {
+      orderSummary = <Spinner />;
     }
 
     return (
       <Aux>
         <Modal show={purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary 
-          ingredients={ingredients} 
-          purchaseCancelled={this.purchaseCancelHandler}
-          purchaseContinued={this.purchaseContinueHandler} 
-          price={totalPrice}/>
+          {orderSummary}
         </Modal>
         <Burger ingredients={ingredients} />
 
