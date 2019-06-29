a41717c54d2eb316a882e10ae584a1ade3ff42ce
Kamran Ali <kamranalinitb@gmail.com>
2019-06-29 11:58:41 +0530

    Persisting Auth State

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Route, Switch, withRouter} from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
@@ -7,18 +9,35 @@ import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 
-const App = () => (
-    <div>
-      <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" component={BurgerBuilder} />
-        </Switch>
-      </Layout>
-    </div>
-  );
+import * as actions from './store/actions/index';
 
-export default App;
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    return (
+        <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    )
+  }
+}
+
+  const mapDispatchToProps = dispatch => {
+    return {
+      onTryAutoSignup: (ingName) => dispatch(actions.authCheckState())
+    }
+  }
+
+export default withRouter(connect(null, mapDispatchToProps)(App)); 
