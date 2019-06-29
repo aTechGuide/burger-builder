7c5eed452912af514e94c78f8cfcb3bc0c0b1b0a
Kamran Ali <kamranalinitb@gmail.com>
2019-06-29 12:13:28 +0530

    Guarding the routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
@@ -18,26 +18,47 @@ class App extends React.Component {
   }
 
   render() {
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" component={BurgerBuilder} />
+        <Redirect to="/"/>
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" component={BurgerBuilder} />
+          <Redirect to="/"/>
+      </Switch>
+      );
+    }
+
     return (
         <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     )
   }
 }
 
-  const mapDispatchToProps = dispatch => {
-    return {
-      onTryAutoSignup: (ingName) => dispatch(actions.authCheckState())
-    }
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
+}
 
-export default withRouter(connect(null, mapDispatchToProps)(App)); 
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: (ingName) => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App)); 
